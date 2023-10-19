58900a897177996603ee54400f96318fd91a0d8b
NipunLiyanage69 <nipun.rightmo@gmail.com>
2023-10-19 20:53:51 +0530

    Redesign UI

--- resources/views/livewire/user-card.blade.php
+++ resources/views/livewire/user-card.blade.php
@@ -1,33 +1,33 @@
-<div class="flex justify-center items-center border border-gray-500">
-    <div class="card w-full max-w-sm">
+<div class="flex justify-center items-center border border-gray-300 m-3 p-3 rounded-lg shadow-lg">
+    <div class="card w-full text-center">
         <div class="card-body">
             <div class="flex flex-col items-center">
                 <img src="{{ $user->profile_photo_path }}" alt="Profile image" class="rounded-full w-32 h-32">
                 <h2 class="mt-4 text-xl font-semibold">{{ $user->name }}</h2>
                 <p class="text-gray-500">{{ $user->id }}</p>
+
+                <ul class="mt-4">
+                    <li class="flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M16 12a4 4 0 11-8 0 4 4 0 018 0zM7 20h5v4m0-4h5v4m-5-4v-4h5" />
+                        </svg>
+                        <span class="ml-2">{{ $user->email }}</span>
+                    </li>
+                    <li class="flex items-center mt-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M12 15v2m-6 2v2m0-4h6m2 4h6m6-4v2m0-4v2m0-4h-2m0 4h-2m0-4h-2" />
+                        </svg>
+                        <span class="ml-2">
+                            <{{ $user->password }} /span>
+                    </li>
+                </ul>
+                <button class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 hover:shadow-lg"
+                    wire:click.prevent="deleteUser">Delete</button>
             </div>
-            <ul class="mt-4">
-                <li class="flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400" fill="none"
-                        viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M16 12a4 4 0 11-8 0 4 4 0 018 0zM7 20h5v4m0-4h5v4m-5-4v-4h5" />
-                    </svg>
-                    <span class="ml-2">{{ $user->email }}</span>
-                </li>
-                <li class="flex items-center mt-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400" fill="none"
-                        viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M12 15v2m-6 2v2m0-4h6m2 4h6m6-4v2m0-4v2m0-4h-2m0 4h-2m0-4h-2" />
-                    </svg>
-                    <span class="ml-2">
-                        <{{ $user->password }} /span>
-                </li>
-                <li>
-                    <button class="bg-red-500 text-white px-4 py-2 rounded" wire:click.prevent="deleteUser">Delete</button>
-                </li>
-            </ul>
         </div>
     </div>
 </div>
--- resources/views/livewire/users-list.blade.php
+++ resources/views/livewire/users-list.blade.php
@@ -2,14 +2,9 @@
     @foreach ($users as $user)
         @livewire('user-card', ['user' => $user], key($user->id))
     @endforeach
-    @if($hasMorePages)
-    <div
-        x-data
-        x-intersect="@this.call('loadUsers')"
-        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-4">
-            @foreach(range(1, 4) as $x)
-                <div class="bg-gray-100 w-100 text-hide">Loading...</div>
-            @endforeach
+    @if ($hasMorePages)
+        <div x-data x-intersect="@this.call('loadUsers')" class="flex justify-center items-center">
+            <div class="text-center w-full m-4 p-4 text-lg font-bold bg-gray-300">Loading...</div>
         </div>
     @endif
 </div>
